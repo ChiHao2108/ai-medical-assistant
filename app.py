8ea90620a6d10bd85f5913b9ea1f17b0359ffb53
ChiHao <chihao@example.com>
2025-06-16 22:52:03 +0700

    Initial commit

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def analyze_image():
         return jsonify({"error": f"Có lỗi xảy ra trong quá trình phân tích hình ảnh: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
